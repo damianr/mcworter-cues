c13f3439029b79c0ad8ce6966830bc48f3d4ae3f
Damian Rebman <damian.rebman@gmail.com>
2025-09-30 15:24:10 -0700

    remove scale on hover

--- components/CueSection.vue
+++ components/CueSection.vue
@@ -3,11 +3,10 @@
     <div
       v-for="cue in cues"
       :key="cue.id"
-      class="cursor-pointer relative group/item transition-opacity duration-300"
+      class="cursor-pointer mb-24 relative group/item transition-opacity duration-300"
       :class="{
-        'opacity-50': hoveredCueId !== null && hoveredCueId !== cue.id,
+        'opacity-75': hoveredCueId !== null && hoveredCueId !== cue.id,
         'opacity-100': hoveredCueId === null || hoveredCueId === cue.id,
-        'mb-24': addBottomMargin,
       }"
       @click="navigateToCue(cue.id)"
       @mouseenter="hoveredCueId = cue.id"
@@ -18,14 +17,14 @@
         <NuxtImg
           :src="cue.images[1]"
           :alt="cue.title"
-          class="w-full object-fill group-hover/item:scale-[1.01] transition-all duration-300 drop-shadow-[0_0_12px_rgba(255,255,255,0.05)] group-hover/item:drop-shadow-[0_0_24px_rgba(255,255,255,0.1)]"
+          class="w-full object-fill transition-all duration-400 drop-shadow-[0_0_12px_rgba(255,255,255,0.05)] group-hover/item:drop-shadow-[0_0_24px_rgba(255,255,255,0.1)]"
           loading="lazy"
         />
       </div>
 
       <!-- Content -->
       <div
-        class="absolute top-[50%] right-8 group-hover/item:opacity-100 group-hover/item:top-[40%] opacity-0 transition-all"
+        class="absolute top-[50%] right-8 group-hover/item:opacity-100 group-hover/item:top-[40%] opacity-0 transition-all duration-400"
       >
         <h4 class="text-base font-bold text-ink mb-0 leading-none">
           {{ cue.title }}
